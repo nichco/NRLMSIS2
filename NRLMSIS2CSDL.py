6c2ac8f54fd5d41398eddf11ff118194156cc0db
Nicholas Orndorff <norndorff@live.com>
2023-07-13 12:28:24 -0700

    the smt model works

--- NRLMSIS2CSDL.py
+++ NRLMSIS2CSDL.py
@@ -2,7 +2,7 @@ import csdl
 import python_csdl_backend
 import numpy as np
 import pickle
-from smt.surrogate_models import RBF, RMTB, KRG
+from smt.surrogate_models import RBF
 import matplotlib.pyplot as plt
 plt.rcParams.update(plt.rcParamsDefault)
 
@@ -13,29 +13,46 @@ rho_data = pickle.load(file)
 eps = 1E-12
 alt = np.arange(0, 1000 + eps, 10)
 
-coefs = np.polyfit(alt, rho_data, 12)
+sm = RBF(d0=50, print_global=False, print_solver=False,)
+sm.set_training_values(alt, rho_data)
+sm.train()
 
 
 class Atm(csdl.Model):
     def initialize(self):
         pass
     def define(self):
-        h = self.declare_variable('altitude', val=0)
+        altitude = self.declare_variable('altitude', val=0)
 
-        #px = p[0]*x**(N-1) + p[1]*x**(N-2) + p[2]*x**(N-3) + p[3]*x**(N-4) + p[4]*x**(N-5)...
-        temp = self.create_output('temp', shape=(len(coefs)), val=0)
-        for i in range(len(coefs)):
-            temp[i] = coefs[i]*h**(len(coefs) - 1 - i)
-
-        rho = csdl.sum(temp)
+        rho = csdl.custom(altitude, op=AtmExplicit())
         self.register_output('rho', rho)
+
+class AtmExplicit(csdl.CustomExplicitOperation):
+    def initialize(self):
+        pass
+    def define(self):
+        self.add_input('altitude')
+        self.add_output('rho')
+        self.declare_derivatives('rho', 'altitude')
+
+    def compute(self, inputs, outputs):
+        outputs['rho'] = sm.predict_values(inputs['altitude'])
+
+    def compute_derivatives(self, inputs, derivatives):
+        drdh = sm.predict_derivatives(inputs['altitude'], 0)
+        derivatives['rho', 'altitude'] = drdh
+
+
+
+
 
 
 
 if __name__ == '__main__':
 
-    data = np.zeros((len(alt)))
-    for i, h in enumerate(alt):
+    z = np.arange(0, 1000 + eps, 1)
+    data = np.zeros((len(z)))
+    for i, h in enumerate(z):
 
         sim = python_csdl_backend.Simulator(Atm())
         sim['altitude'] = h
@@ -46,6 +63,8 @@ if __name__ == '__main__':
     #print(sim['rho'])
 
 
-    plt.plot(alt, rho_data)
-    plt.plot(alt, data)
+    plt.plot(alt, rho_data, color='limegreen',zorder=0)
+    plt.scatter(alt, rho_data,s=40,zorder=5,color='purple',edgecolors='black',linewidth=0.5,alpha=0.5)
+    plt.plot(z, data, color='purple',zorder=10)
+    # plt.xlim(0,200)
     plt.show()
