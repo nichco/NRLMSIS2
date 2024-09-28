acd62f9570b1834c41856fd88f9c4f32f512a64e
Nicholas Orndorff <norndorff@live.com>
2024-09-28 13:26:50 -0700

    reformat the repo

--- akima_test.py
+++ akima_test.py
@@ -5,27 +5,35 @@ from scipy.interpolate import Akima1DInterpolator
 import matplotlib.pyplot as plt
 
 
-file = open('density_0_149_1.pkl', 'rb')
+file = open('density_0_1000_1.pkl', 'rb')
 density = pickle.load(file)
 
-file = open('altitude_0_149_1.pkl', 'rb')
+file = open('altitude_0_1000_1.pkl', 'rb')
 altitude = pickle.load(file)
 
 
 
 xs = np.linspace(min(altitude), max(altitude), 300)
-y_akima = Akima1DInterpolator(altitude, density, method="akima")(xs)
-y_makima = Akima1DInterpolator(altitude, density, method="makima")(xs)
+akima = Akima1DInterpolator(altitude, density, method="akima")
+y_akima = akima(xs)
 
 
+y_akima_der = Akima1DInterpolator.derivative(akima)(xs)
 
-plt.plot(altitude, density, label='data')
-plt.plot(xs, y_akima, label='akima')
-plt.plot(xs, y_makima, label='makima')
+
+plt.plot(altitude, density, label='NRLMSIS2.0 data')
+plt.plot(xs, y_akima, label='akima fit')
+# plt.plot(xs, y_akima_der, label='derivative')
 
 plt.yscale('log')
 plt.xlabel('Altitude (m)')
 plt.ylabel('Density (kg/m^3)')
 plt.legend()
 plt.grid()
+plt.xlim(min(altitude), max(altitude))
+plt.show()
+
+
+
+plt.plot(xs, y_akima_der, label='derivative')
 plt.show()
--- data_extraction.py
+++ data_extraction.py
@@ -1,13 +1,52 @@
 import pandas as pd
 import pickle
+import numpy as np
 
 
 # Read the Excel file
-df = pd.read_excel('0_149.xlsx')
+df_0_149 = pd.read_excel('0_149.xlsx')
+df_150_299 = pd.read_excel('150_299.xlsx')
+df_300_449 = pd.read_excel('300_449.xlsx')
+df_450_599 = pd.read_excel('450_599.xlsx')
+df_600_749 = pd.read_excel('600_749.xlsx')
+df_750_899 = pd.read_excel('750_899.xlsx')
+df_900_1000 = pd.read_excel('900_1000.xlsx')
 
-altitude = pd.to_numeric(df.iloc[1:, 5]).to_numpy() # km
-temperature = pd.to_numeric(df.iloc[1:, 12]).to_numpy() # K
-density = pd.to_numeric(df.iloc[1:, 11]).to_numpy() # g / cm^3
+altitude_0_149 = pd.to_numeric(df_0_149.iloc[1:, 5]).to_numpy() # km
+temperature_0_149 = pd.to_numeric(df_0_149.iloc[1:, 12]).to_numpy() # K
+density_0_149 = pd.to_numeric(df_0_149.iloc[1:, 11]).to_numpy() # g / cm^3
+
+altitude_150_299 = pd.to_numeric(df_150_299.iloc[1:, 5]).to_numpy() # km
+temperature_150_299 = pd.to_numeric(df_150_299.iloc[1:, 12]).to_numpy() # K
+density_150_299 = pd.to_numeric(df_150_299.iloc[1:, 11]).to_numpy() # g / cm^3
+
+altitude_300_449 = pd.to_numeric(df_300_449.iloc[1:, 5]).to_numpy() # km
+temperature_300_449 = pd.to_numeric(df_300_449.iloc[1:, 12]).to_numpy() # K
+density_300_449 = pd.to_numeric(df_300_449.iloc[1:, 11]).to_numpy() # g / cm^3
+
+altitude_450_599 = pd.to_numeric(df_450_599.iloc[1:, 5]).to_numpy() # km
+temperature_450_599 = pd.to_numeric(df_450_599.iloc[1:, 12]).to_numpy() # K
+density_450_599 = pd.to_numeric(df_450_599.iloc[1:, 11]).to_numpy() # g / cm^3
+
+altitude_600_749 = pd.to_numeric(df_600_749.iloc[1:, 5]).to_numpy() # km
+temperature_600_749 = pd.to_numeric(df_600_749.iloc[1:, 12]).to_numpy() # K
+density_600_749 = pd.to_numeric(df_600_749.iloc[1:, 11]).to_numpy() # g / cm^3
+
+altitude_750_899 = pd.to_numeric(df_750_899.iloc[1:, 5]).to_numpy() # km
+temperature_750_899 = pd.to_numeric(df_750_899.iloc[1:, 12]).to_numpy() # K
+density_750_899 = pd.to_numeric(df_750_899.iloc[1:, 11]).to_numpy() # g / cm^3
+
+altitude_900_1000 = pd.to_numeric(df_900_1000.iloc[1:, 5]).to_numpy() # km
+temperature_900_1000 = pd.to_numeric(df_900_1000.iloc[1:, 12]).to_numpy() # K
+density_900_1000 = pd.to_numeric(df_900_1000.iloc[1:, 11]).to_numpy() # g / cm^3
+
+
+
+# Concatenate the data
+altitude = np.concatenate((altitude_0_149, altitude_150_299, altitude_300_449, altitude_450_599, altitude_600_749, altitude_750_899, altitude_900_1000))
+temperature = np.concatenate((temperature_0_149, temperature_150_299, temperature_300_449, temperature_450_599, temperature_600_749, temperature_750_899, temperature_900_1000))
+density = np.concatenate((density_0_149, density_150_299, density_300_449, density_450_599, density_600_749, density_750_899, density_900_1000))
+
 
 # unit conversions
 altitude = altitude * 1e3 # m
@@ -19,11 +58,11 @@ print('temperature: ', temperature)
 print('density: ', density)
 
 
-with open('altitude_0_149_1.pkl', 'wb') as f:
+with open('altitude_0_1000_1.pkl', 'wb') as f:
     pickle.dump(altitude, f)
 
-with open('temperature_0_149_1.pkl', 'wb') as f:
+with open('temperature_0_1000_1.pkl', 'wb') as f:
     pickle.dump(temperature, f)
 
-with open('density_0_149_1.pkl', 'wb') as f:
+with open('density_0_1000_1.pkl', 'wb') as f:
     pickle.dump(density, f)
